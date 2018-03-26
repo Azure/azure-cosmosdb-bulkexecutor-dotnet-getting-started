81a8b04ad62fb87a1bb7df891d52956a43e4bd47
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2018-03-25 21:28:34 -0700

    First commit containing BulkImport sample

---
+++ BulkImportSample/BulkImportSample/Program.cs
@@ -0,0 +1,238 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace BulkImportSample
+{
+    using System;
+    using System.Configuration;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.CosmosDB.BulkExecutor;
+    using Microsoft.Azure.CosmosDB.BulkExecutor.BulkImport;
+
+    class Program
+    {
+        private static readonly string EndpointUrl = ConfigurationManager.AppSettings["EndPointUrl"];
+        private static readonly string AuthorizationKey = ConfigurationManager.AppSettings["AuthorizationKey"];
+        private static readonly string DatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
+        private static readonly string CollectionName = ConfigurationManager.AppSettings["CollectionName"];
+        private static readonly int CollectionThroughput = int.Parse(ConfigurationManager.AppSettings["CollectionThroughput"]);
+
+        private static readonly ConnectionPolicy ConnectionPolicy = new ConnectionPolicy
+        {
+            ConnectionMode = ConnectionMode.Direct,
+            ConnectionProtocol = Protocol.Tcp
+        };
+
+        private DocumentClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        private Program(DocumentClient client)
+        {
+            this.client = client;
+        }
+
+        public static void Main(string[] args)
+        {
+            Trace.WriteLine("Summary:");
+            Trace.WriteLine("--------------------------------------------------------------------- ");
+            Trace.WriteLine(String.Format("Endpoint: {0}", EndpointUrl));
+            Trace.WriteLine(String.Format("Collection : {0}.{1}", DatabaseName, CollectionName));
+            Trace.WriteLine("--------------------------------------------------------------------- ");
+            Trace.WriteLine("");
+
+            try
+            {
+                using (var client = new DocumentClient(
+                    new Uri(EndpointUrl),
+                    AuthorizationKey,
+                    ConnectionPolicy))
+                {
+                    var program = new Program(client);
+                    program.RunBulkImportAsync().Wait();
+                }
+            }
+            catch (AggregateException e)
+            {
+                Trace.TraceError("Caught AggregateException in Main, Inner Exception:\n" + e);
+                Console.ReadKey();
+            }
+
+        }
+
+        /// <summary>
+        /// Driver function for bulk import.
+        /// </summary>
+        /// <returns></returns>
+        private async Task RunBulkImportAsync()
+        {
+            // Cleanup on start if set in config.
+
+            DocumentCollection dataCollection = null;
+            try
+            {
+                if (bool.Parse(ConfigurationManager.AppSettings["ShouldCleanupOnStart"]))
+                {
+                    Database database = Utils.GetDatabaseIfExists(client, DatabaseName);
+                    if (database != null)
+                    {
+                        await client.DeleteDatabaseAsync(database.SelfLink);
+                    }
+
+                    Trace.TraceInformation("Creating database {0}", DatabaseName);
+                    database = await client.CreateDatabaseAsync(new Database { Id = DatabaseName });
+
+                    Trace.TraceInformation(String.Format("Creating collection {0} with {1} RU/s", CollectionName, CollectionThroughput));
+                    dataCollection = await Utils.CreatePartitionedCollectionAsync(client, DatabaseName, CollectionName, CollectionThroughput);
+                }
+                else
+                {
+                    dataCollection = Utils.GetCollectionIfExists(client, DatabaseName, CollectionName);
+                    if (dataCollection == null)
+                    {
+                        throw new Exception("The data collection does not exist");
+                    }
+                }
+            }
+            catch (Exception de)
+            {
+                Trace.TraceError("Unable to initialize, exception message: {0}", de.Message);
+                throw;
+            }
+
+            // Prepare for bulk import.
+
+            // Creating documents with simple partition key here.
+            string partitionKeyProperty = dataCollection.PartitionKey.Paths[0].Replace("/", "");
+
+            string sampleDocument = File.ReadAllText("../../" + ConfigurationManager.AppSettings["SampleDocumentTemplateFile"]);
+            Dictionary<string, object> sampleDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(sampleDocument);
+
+            long numberOfDocumentsToGenerate = long.Parse(ConfigurationManager.AppSettings["NumberOfDocumentsToImport"]);
+            int numberOfBatches = int.Parse(ConfigurationManager.AppSettings["NumberOfBatches"]);
+            long numberOfDocumentsPerBatch = (long)Math.Floor(((double)numberOfDocumentsToGenerate) / numberOfBatches);
+
+            IBulkExecutor bulkExecutor = new BulkExecutor(client, dataCollection);
+            await bulkExecutor.InitializeAsync();
+
+            BulkImportResponse bulkImportResponse = null;
+            long totalNumberOfDocumentsInserted = 0;
+            double totalRequestUnitsConsumed = 0;
+            double totalTimeTakenSec = 0;
+
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            for (int i = 0; i < numberOfBatches; i++)
+            {
+                // Generate documents to import.
+
+                List<object> documentsToImportInBatch = new List<object>();
+                long prefix = i * numberOfDocumentsPerBatch;
+
+                Trace.TraceInformation(String.Format("Generating {0} documents to import for batch {1}", numberOfDocumentsPerBatch, i));
+                for (int j = 0; j < numberOfDocumentsPerBatch; j++)
+                {
+                    string partitionKeyValue = (prefix + j).ToString();
+                    sampleDictionary["id"] = partitionKeyValue + Guid.NewGuid().ToString();
+                    sampleDictionary[partitionKeyProperty] = partitionKeyValue;
+
+                    documentsToImportInBatch.Add(sampleDictionary.ToDictionary(entry => entry.Key, entry => entry.Value));
+                }
+
+                // Invoke bulk import (deserialized docs) API.
+
+                var tasks = new List<Task>();
+
+                tasks.Add(Task.Run(async () =>
+                {
+                    Trace.TraceInformation(String.Format("Executing bulk import for batch {0}", i));
+                    do
+                    {
+                        try
+                        {
+                            bulkImportResponse = await bulkExecutor.BulkImportAsync(
+                                documents: documentsToImportInBatch,
+                                enableUpsert: false,
+                                disableAutomaticIdGeneration: true,
+                                maxConcurrencyPerPartitionKeyRange: null,
+                                cancellationToken: token);
+                        }
+                        catch (DocumentClientException de)
+                        {
+                            Trace.TraceError("Document client exception: {0}", de);
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Trace.TraceError("Exception: {0}", e);
+                            break;
+                        }
+                    } while (bulkImportResponse.NumberOfDocumentsImported < documentsToImportInBatch.Count);
+
+                    Trace.WriteLine(String.Format("\nSummary for batch {0}:", i));
+                    Trace.WriteLine("--------------------------------------------------------------------- ");
+                    Trace.WriteLine(String.Format("Inserted {0} docs @ {1} writes/s, {2} RU/s in {3} sec)",
+                        bulkImportResponse.NumberOfDocumentsImported,
+                        Math.Round(bulkImportResponse.NumberOfDocumentsImported / bulkImportResponse.TotalTimeTaken.TotalSeconds),
+                        Math.Round(bulkImportResponse.TotalRequestUnitsConsumed / bulkImportResponse.TotalTimeTaken.TotalSeconds),
+                        bulkImportResponse.TotalTimeTaken.TotalSeconds));
+                    Trace.WriteLine(String.Format("Average RU consumption per document: {0}",
+                        (bulkImportResponse.TotalRequestUnitsConsumed / bulkImportResponse.NumberOfDocumentsImported)));
+                    Trace.WriteLine("---------------------------------------------------------------------\n ");
+
+                    totalNumberOfDocumentsInserted += bulkImportResponse.NumberOfDocumentsImported;
+                    totalRequestUnitsConsumed += bulkImportResponse.TotalRequestUnitsConsumed;
+                    totalTimeTakenSec += bulkImportResponse.TotalTimeTaken.TotalSeconds;
+                },
+                token));
+
+                tasks.Add(Task.Run(() =>
+                {
+                    char ch = Console.ReadKey(true).KeyChar;
+                    if (ch == 'c' || ch == 'C')
+                    {
+                        tokenSource.Cancel();
+                        Trace.WriteLine("\nTask cancellation requested.");
+                    }
+                }));
+
+                await Task.WhenAll(tasks);
+            }
+
+            Trace.WriteLine("Overall summary:");
+            Trace.WriteLine("--------------------------------------------------------------------- ");
+            Trace.WriteLine(String.Format("Inserted {0} docs @ {1} writes/s, {2} RU/s in {3} sec)",
+                totalNumberOfDocumentsInserted,
+                Math.Round(totalNumberOfDocumentsInserted / totalTimeTakenSec),
+                Math.Round(totalRequestUnitsConsumed / totalTimeTakenSec),
+                totalTimeTakenSec));
+            Trace.WriteLine(String.Format("Average RU consumption per document: {0}",
+                (totalRequestUnitsConsumed / totalNumberOfDocumentsInserted)));
+            Trace.WriteLine("--------------------------------------------------------------------- ");
+
+            // Cleanup on finish if set in config.
+
+            if (bool.Parse(ConfigurationManager.AppSettings["ShouldCleanupOnFinish"]))
+            {
+                Trace.TraceInformation("Deleting Database {0}", DatabaseName);
+                await client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseName));
+            }
+
+            Trace.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-TODO: Create a file called LICENSE (not LICENSE.TXT, LICENSE.md, etc.)…
+The Cosmos DB BulkExecutor library for .NET acts as an extension library to the [Cosmos DB .NET SDK](https://docs.microsoft.com/en-us/azure/cosmos-db/sql-api-sdk-dotnet) and provides developers out-of-the-box functionality to perform bulk operations in Cosmos DB.
+
+### Bulk Import API
+
+#### Implementation details
+
+For bulk import, we utilize a system-registered bulk import stored procedure similar to our provided [example](https://github.com/Azure/azure-documentdb-js-server/blob/master/samples/stored-procedures/BulkImport.js) but with an internal optimization to extend its scope of transactionality to a whole partition key range and not a single partition key - it accepts a batch of documents corresponding to a partition key range and creates/upserts documents on the server-side.
+
+When a bulk import API is triggered with a batch of documents, on the client-side, they are first shuffled into buckets corresponding to their target Cosmos DB partition key range. Within each partiton key range bucket, they are broken down into mini-batches - each of which acts as payload to one stored procedure execution. 
+
+We have built in optimizations for the concurrent execution of these mini-batches both within and across partition key ranges to maximally utilize the allocated collection throughput. We have designed an [AIMD-style congestion control](https://en.wikipedia.org/wiki/Additive_increase/multiplicative_decrease) mechanism for each Cosmos DB partition key range **to efficiently handle throttling and timeouts**.
+
+#### API signature & repsonse details
+
+We provide two overloads of the bulk import API - one which accepts a list of JSON-serialized documents and the other a list of deserialized POCO documents.
+
+* With list of JSON-serialized documents
+```csharp
+        Task<BulkImportResponse> BulkImportAsync(
+            IEnumerable<string> documents,
+            bool enableUpsert = false,
+            bool disableAutomaticIdGeneration = true,
+            int? maxConcurrencyPerPartitionKeyRange = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+```
+
+* With list of deserialized POCO documents
+```csharp
+        Task<BulkImportResponse> BulkImportAsync(
+            IEnumerable<object> documents,
+            bool enableUpsert = false,
+            bool disableAutomaticIdGeneration = true,
+            int? maxConcurrencyPerPartitionKeyRange = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+```
+
+##### Configurable parameters:
+* *enableUpsert* : A flag to enable upsert of the documents, default value is false.
+* *disableAutomaticIdGeneration* : A flag to disable automatic generation of ids if absent in the docuement.
+* *maxConcurrencyPerPartitionKeyRange* : The maximum degree of concurrency per partition key range, setting to null will cause library to use default value of 20.
+* *cancellationToken* : The cancellation token to gracefully exit bulk import.
+
+##### Bulk import response object definition
+
+The result of the bulk import API call contains the following attributes:
+* *NumberOfDocumentsImported* (long) : The total number of documents which were successfully imported out of the documents supplied to the bulk import API call.
+* *TotalRequestUnitsConsumed* (double) : The total request units (RU) consumed by the bulk import API call.
+* *TotalTimeTaken* (TimeSpan) : The total time taken by the bulk import API call to complete.
+* *BadInputDocuments* (List\<object\>) : The list of bad-format documents which were not successfully imported in the bulk import API call. User needs to fix the documents - potential reasons could be null document id, invalid JSON, etc.
+
+#### Getting started
+
+You can find a sample application program consuming the bulk import API [here](BulkImportSample\BulkImportSample\Program.cs) - which generates random documents to be then bulk imported into a Cosmos DB collecition. You can configure the application settings in *appSettings* [here](BulkImportSample\BulkImportSample\App.config).
+
+### Couple of other pointers
+
+* For best performance, run your application from an Azure VM in the same region as your Cosmos DB account write region.
+* It is advised to instantiate a single *BulkExecutor* object for the entirety of the application corresponding to a specific Cosmos DB collection.
+* Since a single bulk import API execution consumes a large chunk of the client machine's CPU and network IO by spawning multiple tasks internally, avoid spawning multiple concurrent tasks within your application process each executing bulk import API calls. If a single bulk import API call running on a single VM is unable to consume your entire collection's throughput (if your collections throughput > 1 million RU/s), preferably spin up separate VMs to concurrently execute bulk import API calls.
+* Ensure *InitializeAsync()* is invoked after instantiating a *BulkExecutor* object to fetch the target Cosmos DB collection partition map.
+* In your application's *App.Config*, ensure **gcServer** is enabled for better performance
+```csharp
+	<runtime>
+		<gcServer enabled="true" />
+	</runtime>
+```
+* The library emits traces which can be collected either into a log file or on the console. To enable both, add the following to your application's *App.Config*.
+```csharp
+  <system.diagnostics>
+      <trace autoflush="false" indentsize="4">
+          <listeners>
+              <add name="logListener" type="System.Diagnostics.TextWriterTraceListener" initializeData="application.log" />
+              <add name="consoleListener" type="System.Diagnostics.ConsoleTraceListener" />
+          </listeners>
+      </trace>
+  </system.diagnostics>
+```
+
+Contact [ramkris@microsoft.com](mailto:ramkris@microsoft.com) if you have any queries/feedback.
+
+### Contributing & feedback
+
+This project has adopted the [Microsoft Open Source Code of
+Conduct](https://opensource.microsoft.com/codeofconduct/).  For more information
+see the [Code of Conduct
+FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact
+[opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional
+questions or comments.
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) for contribution guidelines.
+
+To give feedback and/or report an issue, open a [GitHub
+Issue](https://help.github.com/articles/creating-an-issue/).
+
+### Other relevant projects
+
+* [Cosmos DB BulkExecutor library for Java](https://github.com/Azure/azure-cosmosdb-bulkexecutor-java-getting-started)
